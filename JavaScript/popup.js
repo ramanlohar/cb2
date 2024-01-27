3ab259b2b73e894dd1d54d76ac8e5e7977ba0c3b
ramanlohar <ramanlohar11@gmail.com>
2024-01-27 15:48:31 +0530

    new

---
+++ JavaScript/popup.js
@@ -0,0 +1,10 @@
+function openPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+  }
+  
+  function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+  }
+  
